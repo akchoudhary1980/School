f8376532a5dda1408ef095fdbb7416c5c35d7520
akchoudhary1980 <akchoudhary1980@rediffmail.com>
2022-01-04 18:32:25 +0530

    Fees Structure

--- School/wwwroot/jquery-app/dt-fees-structure.js
+++ School/wwwroot/jquery-app/dt-fees-structure.js
@@ -4,7 +4,7 @@
         "serverSide": true,
         "filter": true,
         "ajax": {
-            "url": "/Admin/Staff/GetIndex",
+            "url": "/Admin/FeesStructure/GetIndex",
             "type": "POST",
             "datatype": "json"
         },
@@ -14,26 +14,34 @@
             "searchable": false
         }],
         "columns": [
-            { "data": "staffID", "name": "staffID", "autoWidth": true }, 
-            { "data": "name", "name": "name", "autoWidth": true },
-            { "data": "desginationName", "name": "desginationName", "autoWidth": true },    
-            { "data": "city", "name": "city", "autoWidth": true },  
-            { "data": "mobile", "name": "mobile", "autoWidth": true }, 
+            { "data": "feesStructureID", "name": "feesStructureID", "autoWidth": true },
 
             {
+                "data": "picture", "name": "picture",
+
+                "render": function (data, type, full, meta) {
+                    return "<img src='../UploadFiles/" + full.picture + "' height='50'/>";
+                },
+                "orderable": false,
+                "searchable": false
+            },
+
+            { "data": "className", "name": "className", "autoWidth": true },
+           
+            {
                 "render": function (data, type, full) {
-                    return "<a href='Staff/View?id=" + full.staffID + "' class='btn btn-xs btn-outline-info'><i class='fas fa-search-plus'></i></a>";
+                    return "<a href='FeesStructure/View?id=" + full.feesStructureID + "' class='btn btn-xs btn-outline-info'><i class='fas fa-search-plus'></i></a>";
                 }
             },
 
             {
                 "render": function (data, type, full) {
-                    return "<a href='Staff/Edit?id=" + full.staffID + "' class='btn btn-xs btn-outline-success'><i class='fas fa-edit'></i></a>";
+                    return "<a href='FeesStructure/Edit?id=" + full.feesStructureID + "' class='btn btn-xs btn-outline-success'><i class='fas fa-edit'></i></a>";
                 }
             },
             {
                 "render": function (data, type, full) {
-                    return "<a href='Staff/Delete?id=" + full.staffID + "' class='btn btn-xs btn-outline-danger'><i class='fas fa-window-close'></i></a>";
+                    return "<a href='FeesStructure/Delete?id=" + full.feesStructureID + "' class='btn btn-xs btn-outline-danger'><i class='fas fa-window-close'></i></a>";
                 }
             }, 
            
