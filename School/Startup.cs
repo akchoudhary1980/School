7304717c98b9724c312cd93abd146fb1c6d10ccc
akchoudhary1980 <73695195+akchoudhary1980@users.noreply.github.com>
2022-01-11 08:49:04 +0530

    dsfsd

--- School/Startup.cs
+++ School/Startup.cs
@@ -19,7 +19,7 @@ namespace School
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add services to the
         public void ConfigureServices(IServiceCollection services)
         {
             //JSON Serializer
