8b21386e6d5d3a042821d7d47cf2eca24b15d2b7
akchoudhary1980 <akchoudhary1980@rediffmail.com>
2022-01-15 17:15:11 +0530

    some Work

--- School/Areas/Admission/Controllers/ActivityController.cs
+++ School/Areas/Admission/Controllers/ActivityController.cs
@@ -1,17 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Linq.Dynamic.Core;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
 using School.Areas.Admission.Models;
 
 namespace School.Areas.Admission.Controllers
 {
+    [Area("Admission")]
     public class ActivityController : Controller
-    {
-        [Area("Admission")]
+    {        
         public DBContext db = new DBContext();
         public IActionResult Index()
         {
@@ -34,17 +38,17 @@ namespace School.Areas.Admission.Controllers
         {
             if (ModelState.IsValid)
             {
-                bool duplicate = db.SectionModels.Any(x => x.SectionName == obj.SectionName);
+                bool duplicate = db.ActivityModels.Any(x => x.ActivityName == obj.ActivityName);
                 if (duplicate)
                 {
-                    ModelState.AddModelError("SectionName", "Duplicate Record Found");
+                    ModelState.AddModelError("ActivityName", "Duplicate Record Found");
                     return View();
                 }
                 else
                 {
-                    int incid = db.SectionModels.DefaultIfEmpty().Max(r => r == null ? 0 : r.SectionID);
-                    obj.SectionID = incid + 1;
-                    db.SectionModels.Add(obj);
+                    int incid = db.ActivityModels.DefaultIfEmpty().Max(r => r == null ? 0 : r.ActivityID);
+                    obj.ActivityID = incid + 1;
+                    db.ActivityModels.Add(obj);
                     db.SaveChanges();
                     Response.Cookies.Append("Create", "Yes");
                     return RedirectToAction(nameof(Index));
@@ -57,26 +61,26 @@ namespace School.Areas.Admission.Controllers
         }
         public IActionResult Edit(int id)
         {
-            ViewData["PageTitle"] = "Section Manage";
-            ViewData["PageName"] = "Update Section";
-            ViewData["ControllerName"] = "Section";
-            var model = db.SectionModels.Where(x => x.SectionID == id).FirstOrDefault();
+            ViewData["PageTitle"] = "Activity Manage";
+            ViewData["PageName"] = "Update Activity";
+            ViewData["ControllerName"] = "Activity";
+            var model = db.ActivityModels.Where(x => x.ActivityID == id).FirstOrDefault();
             return View(model);
         }
         [HttpPost]
-        public IActionResult Edit(SectionModel obj)
+        public IActionResult Edit(ActivityModel obj)
         {
             if (ModelState.IsValid)
             {
                 // Check Duplicate and prevet duplication at the time of edit 
                 DBContext db1 = new DBContext();
-                var oldvalue = db1.SectionModels.Where(x => x.SectionID == obj.SectionID).SingleOrDefault();
-                if (oldvalue.SectionName != obj.SectionName)
+                var oldvalue = db1.ActivityModels.Where(x => x.ActivityName == obj.ActivityName).SingleOrDefault();
+                if (oldvalue.ActivityName != obj.ActivityName)
                 {
-                    bool duplicate = db1.SectionModels.Any(x => x.SectionName == obj.SectionName);
+                    bool duplicate = db1.SectionModels.Any(x => x.SectionName == obj.ActivityName);
                     if (duplicate)
                     {
-                        ModelState.AddModelError("SectionName", "Duplicate Record Found");
+                        ModelState.AddModelError("ActivityName", "Duplicate Record Found");
                         return View();
                     }
                     else
@@ -103,18 +107,18 @@ namespace School.Areas.Admission.Controllers
         }
         public IActionResult Delete(int id)
         {
-            ViewData["PageTitle"] = "Section Manage";
-            ViewData["PageName"] = "Delete Section";
-            ViewData["ControllerName"] = "Section";
-            var model = db.SectionModels.Where(x => x.SectionID == id).FirstOrDefault();
+            ViewData["PageTitle"] = "Activity Manage";
+            ViewData["PageName"] = "Delete Activity";
+            ViewData["ControllerName"] = "Activity";
+            var model = db.ActivityModels.Where(x => x.ActivityID == id).FirstOrDefault();
             return View(model);
         }
         [HttpPost]
-        public IActionResult Delete(SectionModel obj, string confirm)
+        public IActionResult Delete(ActivityModel obj, string confirm)
         {
             if (confirm == "Yes")
             {
-                db.SectionModels.RemoveRange(db.SectionModels.Where(x => x.SectionID == obj.SectionID));
+                db.ActivityModels.RemoveRange(db.ActivityModels.Where(x => x.ActivityID == obj.ActivityID));
                 db.SaveChanges();
                 return RedirectToAction(nameof(Index));
             }
--- School/Areas/Admission/Models/ActivityModel.cs
+++ School/Areas/Admission/Models/ActivityModel.cs
@@ -6,7 +6,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace School.Areas.Admin.Models
+namespace School.Areas.Admission.Models
 {
     public class ActivityModel
     {
